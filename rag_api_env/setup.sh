6f40314c9a363cb205cdf7c149f2ce3e7cc510bd
mi92 <michael.d.moor@gmail.com>
2024-02-13 14:24:25 -0800

    added req and setup.sh file

---
+++ rag_api_env/setup.sh
@@ -0,0 +1,9 @@
+# Exact setup: 
+pip install -r requirements.txt
+
+## Packages I installed (minimal setup), potentially in different versions:
+# pip install playwright openai requests ipython pandas numpy
+
+
+
+
