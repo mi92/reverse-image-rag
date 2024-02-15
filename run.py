6f4b78b9f9243f314107039375922929a7c2aaf8
mi92 <michael.d.moor@gmail.com>
2024-02-15 00:44:03 -0800

    added more ex to run.py

--- run.py
+++ run.py
@@ -6,7 +6,8 @@ if __name__ == "__main__":
     openai_api_key = os.getenv("OPENAI_API_KEY")
     api = rir_api.RIR_API(openai_api_key)
 
-    image_url = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSgN8RDkURVE8mgOf-n02TqJdC2l1o5cVFA32NpZtuVp8MaFfZY"
+    #image_url = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSgN8RDkURVE8mgOf-n02TqJdC2l1o5cVFA32NpZtuVp8MaFfZY" # bird
+    image_url = "http://tinyurl.com/2rrws56n" # fractal
     query_text = "What is in this image?"
     response = api.query_with_image(image_url, query_text)
     print(response)
